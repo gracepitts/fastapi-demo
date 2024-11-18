e8a8e0602debeddbc26d3a68c6b1ac6d96547a85
Grace Pitts <gracepitts@Graces-MacBook-Air.local>
2024-11-18 16:37:54 -0500

    data project code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env py
 # python3
-
+import mysql.connector
+from mysql.connector import Error
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
@@ -9,21 +10,51 @@ import os
 
 app = FastAPI()
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Grace!"}
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "twg3sr"
 
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
-@app.get("/square/{a}")
-def square(a: int):
-    return {"square": a * a}
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-    return {"product": c * d}
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/subtract/{e}/{f}")
-def subtract(c: int, d: int):
-    return {"subtract": e - f}
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI app!"}
+
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    
+@app.get('/songs')
+def get_songs():
+    query = "Select * From songs ORDER BY id;"
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(headers, result)))
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
